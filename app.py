5b95188639288603e613a18baf0f2a09e2eb3e54
siriravalid <152952587+siriravalid@users.noreply.github.com>
2024-08-11 18:18:38 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from transformers import pipeline
+try:
+    sentiment_pipeline = pipeline('sentiment-analysis')
+except ImportError as e:
+    st.error(f"Import error: {e}")
+    st.stop()
+st.title("Sentiment Analysis")
+text = st.text_area("Enter text for sentiment analysis")
+if st.button("Analyze"):
+    result = sentiment_pipeline(text)
+    sentiment_label = result[0]['label']
+    
+    if sentiment_label == 'POSITIVE':
+        sentiment = 'Positive'
+    elif sentiment_label == 'NEGATIVE':
+        sentiment = 'Negative'
+    else:
+        sentiment = 'Unknown'
+    
+    st.write(sentiment)
